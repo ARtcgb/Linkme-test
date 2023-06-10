d8d7b6e89033586d34c6f1c61eff0aa5ac462e43
ARtcgb <71631622+ARtcgb@users.noreply.github.com>
2023-06-10 18:54:10 +0800

    Initial commit

---
+++ app/src/main/java/com/ams/linkme/LinkMeApplication.kt
@@ -0,0 +1,22 @@
+package com.ams.linkme
+
+import android.app.Application
+import android.content.Intent
+import com.ams.linkme.ui.activity.LoginActivity
+import com.google.firebase.FirebaseApp
+
+class LinkMeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // 初始化 Firebase
+        FirebaseApp.initializeApp(this)
+
+        // 初始化其他配置或依赖项
+        // 跳转到 LoginActivity
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+}
